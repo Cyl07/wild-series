681513f25044eee7b67f10968f82c6a2324be9fa
Cyl <labetowiez@aol.fr>
2021-12-07 13:29:12 +0100

    k

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CategoryRepository;
+use App\Repository\ProgramRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/category", name="category_")
+ */
+class CategoryController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index(CategoryRepository $categoryRepository): Response
+    {
+        $categories = $categoryRepository->findAll();
+
+        return $this->render("category/index.html.twig", ["categories" => $categories]);
+    }
+
+    /**
+     * @Route("/{categoryName}", name="show")
+     */
+    public function show(ProgramRepository $programRepository, CategoryRepository $categoryRepository, string $categoryName): Response
+    {
+        $category = $categoryRepository->findOneBy(["name" => $categoryName]);
+
+        if (!$category) {
+            throw $this->createNotFoundException(
+                'No category with id : ' . $categoryName . ' found in category\'s table.'
+            );
+        }
+
+        $programs = $programRepository->findBy(
+            ["category" => $category],
+            ["id" => "DESC"],
+            3
+        );
+
+        return $this->render("category/show.html.twig", ["programs" => $programs]);
+    }
+}
--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -27,4 +27,24 @@ class ProgramController extends AbstractController
             'programs' => $programs,
         ]);
     }
+
+    /**
+     * Getting a program by id
+     *
+     * @Route("/show/{id<^[0-9]+$>}", name="show")
+     * @return Response
+     */
+    public function show(ProgramRepository $programRepository, int $id): Response
+    {
+        $program = $programRepository->findOneBy(['id' => $id]);
+
+        if (!$program) {
+            throw $this->createNotFoundException(
+                'No program with id : ' . $id . ' found in program\'s table.'
+            );
+        }
+        return $this->render('program/show.html.twig', [
+            'program' => $program,
+        ]);
+    }
 }
