bf325e42e28e682946ceab62b6d6e45fd1f69f29
Cyl <labetowiez@aol.fr>
2021-12-07 10:17:56 +0100

    ...

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -3,19 +3,28 @@
 
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/program", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/program/", name="program_index")
+     * Show all rows from Program’s entity
+     *
+     * @Route("/", name="index")
+     * @return Response A response instance
      */
-    public function index(): Response
+    public function index(ProgramRepository $programRepository): Response
     {
+        $programs = $programRepository->findAll();
+
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Séries',
+            'programs' => $programs,
         ]);
     }
 }
